953a8c9462fef085510127836b360d86a721ee29
Сергей Сергей <eintyler23rus@gmail.com>
2024-03-07 13:35:19 +0300

    Рабочая версия))

--- Program.cs
+++ Program.cs
@@ -9,46 +9,30 @@ for (int i = 0; i < arrayNums.Length; i++)
 	ints[i] = _parcer(arrayNums[i]);
 }
 
-int _parcer(string s)
+int _parcer(string s)//Метод парсинга строки
 {
 	int.TryParse(s, out int num);
-	if (num == 0) { Console.WriteLine("Ошибка ввода"); return 0; }
+	if (num == 0 & s != "0") { Console.WriteLine("Ошибка ввода"); return 0; }
 	else return num;	
 }
 //заполнение массива с клавиатуры{end}
 //====================================
 //Вывод массива в обратном порядке
 Console.WriteLine("\nВывод массива в обратном порядке");
-for (int i = ints.Length - 1; i >= 0; i--)
-{
-	Console.WriteLine(ints[i]);
-}
+for (int i = ints.Length - 1; i >= 0; i--) Console.WriteLine(ints[i]);
 //Вывод массива в обратном порядке{end}
 //====================================
 //Найти сумму четных элементов массива
 int sum = 0;
 for (int i = 0; i < ints.Length; i++)
 {
-	if (ints[i] % 2 == 0)
-	{
-        sum += ints[i];
-    }
+	if (ints[i] % 2 == 0) sum += ints[i];
 }
 Console.WriteLine("Сумма четных элементов массива равна " + sum);
 //Найти сумму четных элементов массива{end}
 //====================================
 //Найти наименьшее число массива
-int minNumber = 0;
-for (int i = 0;i< ints.Length; i++)
-{
-	while(minNumber == 0)
-	{
-		minNumber = ints[i];
-		if (minNumber > ints[i]) minNumber = ints[i];
-		break;
-	}
-}
-Console.WriteLine("\nHаименьший элемент массива равен " + minNumber);
+Console.WriteLine("\nHаименьший элемент массива равен " + ints.Min());
 Console.ReadLine();
 //Найти наименьшее число массива{end}
 //====================================
